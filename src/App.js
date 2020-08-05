27c4396f1c17028bcd15ae86d8a4ca48361496aa
Kunal <ksevkani495@gmail.com>
2020-08-05 12:29:29 +0530

    infinite scroll functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import colleges from "./data/colleges.json";
+import "./App.css";
 
 function App() {
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    setIsFetching(true);
+  };
+
+  const fetchMoreColleges = () => {
+    setTimeout(() => {
+      setColleges((prevState) => [
+        ...prevState,
+        ...Array.from(Array(20).keys(), (n) => n + prevState.length + 1),
+      ]);
+      setIsFetching(false);
+    }, 2000);
+  };
+
+  const [colleges, setColleges] = useState(
+    Array.from(Array(30).keys(), (n) => n + 1)
+  );
+  const [isFetching, setIsFetching] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  useEffect(() => {
+    if (!isFetching) return;
+    fetchMoreColleges();
+  }, [isFetching]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {colleges.map((college) => (
+        <h1 key={college}>{college}</h1>
+      ))}
+      {isFetching && <p>Loading...</p>}
     </div>
   );
 }
