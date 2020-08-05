b69c0e4a233f24ce15e7bca8320ad57a9f848136
Kunal <ksevkani495@gmail.com>
2020-08-05 14:14:39 +0530

    data added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
 import React, { useState, useEffect } from "react";
-import colleges from "./data/colleges.json";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [colleges, setColleges] = useState([]);
+  const [visibleColleges, setVisibleColleges] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [page, setPage] = useState(1);
+
   const handleScroll = () => {
     if (
       window.innerHeight + document.documentElement.scrollTop !==
       document.documentElement.offsetHeight
     )
       return;
+
     setIsFetching(true);
+    setPage((prevState) => prevState + 1);
   };
 
   const fetchMoreColleges = () => {
+    if (page >= 5) {
+      window.removeEventListener("scroll", handleScroll);
+      setIsFetching(false);
+    }
+
     setTimeout(() => {
-      setColleges((prevState) => [
-        ...prevState,
-        ...Array.from(Array(20).keys(), (n) => n + prevState.length + 1),
-      ]);
+      setVisibleColleges([...colleges.slice(0, page * 10)]);
       setIsFetching(false);
     }, 2000);
   };
 
-  const [colleges, setColleges] = useState(
-    Array.from(Array(30).keys(), (n) => n + 1)
-  );
-  const [isFetching, setIsFetching] = useState(false);
   useEffect(() => {
+    axios.get("./colleges.json").then((res) => {
+      setColleges(res.data.colleges);
+      const data = res.data.colleges.slice(0, 10);
+      setVisibleColleges(data);
+    });
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   useEffect(() => {
     if (!isFetching) return;
     fetchMoreColleges();
-  }, [isFetching]);
+  }, [isFetching, page]);
   return (
     <div>
-      {colleges.map((college) => (
-        <h1 key={college}>{college}</h1>
+      {visibleColleges.map((college) => (
+        <div key={college.college_name} style={{ height: "200px" }}>
+          {college.college_name}
+        </div>
       ))}
       {isFetching && <p>Loading...</p>}
     </div>
